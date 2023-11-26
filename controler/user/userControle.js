3025e549da4c91633b559f8fbd591f2e3f82b2a0
Adarsh nm <adarshnm02@gmail.com>
2023-11-26 21:53:43 +0530

    add login page

--- controler/user/userControle.js
+++ controler/user/userControle.js
@@ -3,8 +3,17 @@ const express = require('express')
 const loadHome = (req,res)=>{
   
         res.render('user/index')
+        
+}
+const login = (req,res)=>{
+        res.render('user/user_login')
 }
 
+const loadShop = (req,res)=>{
+        res.render('user/shop-grid')
+}
+const loadBlog = (req,res)=>{
+        res.render('user/blog')
+}
 
-
-module.exports = loadHome
+module.exports = {loadHome,login,loadShop,loadBlog}
--- routes/userRoute.js
+++ routes/userRoute.js
@@ -4,8 +4,11 @@ const userCtrl = require('../controler/user/userControle')
 // userRot = require('./views/user')
 
 
-user_Route.get('/',userCtrl)
-user_Route.get
+user_Route.get('/',userCtrl.loadHome)
+user_Route.get('/index',userCtrl.loadHome)
+user_Route.get('/login',userCtrl.login)
+user_Route.get('/shop-grid',userCtrl.loadShop)
+user_Route.get('/blog',userCtrl.loadBlog)
 
 
 
