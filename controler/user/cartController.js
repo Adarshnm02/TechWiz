098de309c243b028194427c509743f312be6a4ee
Adarsh nm <adarshnm02@gmail.com>
2023-12-18 22:15:29 +0530

    make changes in cart

--- controler/user/cartController.js
+++ controler/user/cartController.js
@@ -6,140 +6,147 @@ const Product = require("../../models/productModel")
 
 
 module.exports = {
-async load_cart(req, res) {
-    try {
-        if(req.session.user){
-            const userId = req.session.user;
+    async load_cart(req, res) {
+        try {
+            if (req.session.user) {
+                const userId = req.session.user;
+                const user = await User.findById(userId).populate('cart.product')
+                const cart = user.cart;
+
+                console.log(`cart ${cart} user ${user} userId ${userId} cart.productname ${cart.product}`)
+                res.render('user/shoping-cart', { cart, user, session: req.session.user })
+            }
+        } catch (err) {
+            console.log(err);
+            res.render('user/500')
+        }
+    },
+
+
+
+
+    async addToCart(req, res) {
+        const userId = req.session.user;
+        const { product } = req.body;
+
+        try {
+            const pro = await Product.findOne({ _id: product })
             const user = await User.findById(userId).populate('cart.product')
             const cart = user.cart;
+            const incrementAmount = pro.price;
 
-            console.log(`cart ${cart} user ${user} userId ${userId} cart.productname ${cart.product}`)
-            res.render('user/shoping-cart', { cart, user , session : req.session.user})
-        }
-    } catch (err) {
-        console.log(err);
-        res.render('user/500')
-    }
-},
+            const productInTheCart = cart.find(cartItem => cartItem.product.equals(product));
 
-
-
-
-async addToCart (req,res) {
-    const userId = req.session.user;
-    const {product} = req.body;
-
-    try{
-        const pro = await Product.findOne({_id: product})
-        const user = await User.findById(userId).populate('cart.product')
-        const cart = user.cart;
-        const incrementAmount = pro.price;
-
-        const productInTheCart = cart.find(cartItem => cartItem.product.equals(product));
-
-        if(productInTheCart){
-            await User.updateOne(
-                {_id: userId, 'cart.product': product },
-                { $inc: {
-                    'cart.$.quantity': 1,
-                    'cart.$.totalAmount': incrementAmount,
-                    'grandTotal': incrementAmount
-                }}
-            );
-            return res.json({message: "The product quantity incremented"})
-        } else {
-            await User.updateOne(
-                {_id: userId},
-                {
-                    $addToSet: {
-                        cart: {product, quantity: 1, totalAmount: incrementAmount},
-                    },
-                    $inc: {
-                        grandTotal: incrementAmount
+            if (productInTheCart) {
+                await User.updateOne(
+                    { _id: userId, 'cart.product': product },
+                    {
+                        $inc: {
+                            'cart.$.quantity': 1,
+                            'cart.$.totalAmount': incrementAmount,
+                            'grandTotal': incrementAmount
+                        }
                     }
-                }
-            );
-            return res.join({ message: "Product added to Cart"})
-        }
-    }catch(error){
-        console.log("Error in adding the product to the cart", error);
-        res.render('user/500')
-    }
-    
-},
-
-
-async deleteFromCart (req, res){
-    const { product } = req.body;
-    const userId = req.session.user;
-
-    try {
-        const user = await User.findById(userId).populate('cart.product');
-        const removedItem = user.cart.find(cartItem => cartItem.product.equals(product));
-
-        if (removedItem) {
-            const reductionAmount = removedItem.totalAmount;
-
-            await User.updateOne({ _id: userId }, { $pull: { cart: { product: product } } });
-
-            const updatedGrandTotal = user.grandTotal - reductionAmount;
-
-            await User.updateOne({ _id: userId }, { grandTotal: updatedGrandTotal });
-
-            res.json({ message: `Item with ID=${product} removed`, grandTotal: updatedGrandTotal });
-        } else {
-            return res.status(404).json({ message: 'Item not found in the cart' });
-        }
-    } catch (error) {
-        console.error('Error removing product from cart:', error);
-        res.status(500).json({ message: 'Failed to remove product from cart' });
-    }
-},
-
-async qntUpdate (req,res){
-    try{
-        let user = await User.findById(req.session.user)
-        let cart = await user.cart.find(items => items.product._id.toString() === req.params.id)
-        if(cart){
-            const product = await cart.product.findById(cart.product)
-            if(req.body.type === "increment"){
-                if(cart.quantity + 1 > product.stock_count){
-                    return res.send("Insufficient Stock")
-
-                }else{
-                    cart.quantity += 1;
-                    cart.totalAmount = product.price * cart.quantity;
-                }
-            }else if (req.body.type === "decrement"){
-                if(cart.quantity - 1 >= 0){
-                    cart.quantity -= 1;
-                    cart.totalAmount = product.price * cart.quantity;
-                }
-            } 
-
-            let grandTotal = user.cart.forEach(item => totalCartAmount += item.totalAmount )
-            user.grandTotal = totalCartAmount
-
-            await user.save()
-
-            return res.status(200).json({
-                message: "susses",
-                quantity: cart.quantity,
-                totalAmount: cart.totalAmount,
-                grandTotal: cart.grandTotal
-            })
-
-
-        }else{
-            console.log("cart is not found");
+                );
+                return res.json({ message: "The product quantity incremented" })
+            } else {
+                await User.updateOne(
+                    { _id: userId },
+                    {
+                        $addToSet: {
+                            cart: { product, quantity: 1, totalAmount: incrementAmount },
+                        },
+                        $inc: {
+                            grandTotal: incrementAmount
+                        }
+                    }
+                );
+                return res.join({ message: "Product added to Cart" })
+            }
+        } catch (error) {
+            console.log("Error in adding the product to the cart", error);
             res.render('user/500')
         }
 
-    }catch(err){
-        console.log(err);
-        res.render('user/500')
+    },
+
+
+    async deleteFromCart(req, res) {
+        const { product } = req.body;
+        const userId = req.session.user;
+    
+        try {
+            const user = await User.findById(userId).populate('cart.product');
+            const removedItem = await user.cart.find(cartItem => cartItem.product.equals(product));
+    
+            if (removedItem) {
+                const reductionAmount = removedItem.totalAmount;
+    
+                await User.updateOne({ _id: userId }, { $pull: { cart: { product: product } } });
+    
+                const updatedGrandTotal = user.grandTotal - reductionAmount;
+    
+                await User.updateOne({ _id: userId }, { grandTotal: updatedGrandTotal });
+    
+                res.json({ message: `Item with ID=${product} removed`, grandTotal: updatedGrandTotal });
+            } else {
+                return res.status(404).json({ message: 'Item not found in the cart' });
+            }
+        } catch (error) {
+            console.error('Error removing product from cart:', error);
+            res.status(500).json({ message: 'Failed to remove product from cart' });
+        }
+    },
+
+
+    async qntUpdate(req, res) {
+        try {
+            const user = await User.findById(req.session.user); 
+
+          
+            const cartItem = user.cart.find(item => item.product.toString() === req.body.productId);
+
+            if (cartItem) {
+                const product = await Product.findById(cartItem.product); 
+
+                if (req.body.action === "increment") {
+                    if (cartItem.quantity + 1 > product.stock_count) {
+                        return res.status(400).json({ message: "Insufficient Stock" });
+                    } else {
+                        cartItem.quantity += 1;
+                        cartItem.totalAmount = product.price * cartItem.quantity;
+                    }
+                } else if (req.body.action === "decrement") {
+                    if (cartItem.quantity - 1 >= 0) {
+                        cartItem.quantity -= 1;
+                        cartItem.totalAmount = product.price * cartItem.quantity;
+                    }
+                }
+
+                let totalCartAmount = 0;
+                user.cart.forEach(item => {
+                    totalCartAmount += item.totalAmount;
+                });
+                user.grandTotal = totalCartAmount;
+
+                await user.save(); 
+
+                return res.status(200).json({
+                    message: "Success",
+                    quantity: cartItem.quantity,
+                    totalAmount: cartItem.totalAmount,
+                    grandTotal: user.grandTotal
+                });
+            } else {
+                console.log("Cart item not found");
+                return res.status(404).json({ message: "Cart item not found" });
+            }
+        } catch (err) {
+            console.log(err);
+            return res.status(500).json({ message: "Internal Server Error" });
+        }
     }
-}
+};
 
 
 
@@ -147,4 +154,4 @@ async qntUpdate (req,res){
 
 
 
-}
+
--- routes/userRoute.js
+++ routes/userRoute.js
@@ -39,14 +39,14 @@ user_Route.get('/product/Details/:id', Auth.isLogedout, productControl.productDe
 
 //Cart
 user_Route.get('/cart', Auth.isLogedout, cartCtrl.load_cart)
-user_Route.post('/addToCart', cartCtrl.addToCart)
+user_Route.post('/addToCart', Auth.isLogedout, cartCtrl.addToCart)
 user_Route.post('/removeFromCart', cartCtrl.deleteFromCart)
-user_Route.post('/cart/increment/:id', Auth.isLogedout, cartCtrl.qntUpdate)
-user_Route.post('/cart/decrement/:id', Auth.isLogedout, cartCtrl.qntUpdate)
+user_Route.post('/cart/qntUpdate', Auth.isLogedout, cartCtrl.qntUpdate)
+// user_Route.post('/cart/decrement/:id', Auth.isLogedout, cartCtrl.qntUpdate)
 
 
 //checkout
 user_Route.get('/checkout', Auth.isLogedout, checkoutCtrl.loadCheckout)
 
-console.log("from head {checking}");q
+
 module.exports = user_Route;
